1bef13de1b0fcdd0f71a803974697d905d3969b8
Skampo-San <72740149+Nino-Saadi@users.noreply.github.com>
2022-04-11 11:04:39 +0200

    Initialisation et mise en place des packets

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,8 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+require('bootstrap');
+
+require ('@fortawesome/fontawesome-free/css/all.min.css');
+require ('@fontawesome/fontawesome-free/js/all.js');
